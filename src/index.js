f9f39e4617d390fd2055f678b061b0d7a2cf0313
AinaraHF <ainarahf@gmail.com>
2025-02-26 09:12:11 +0100

    CRUD finished

--- src/index.js
+++ src/index.js
@@ -28,7 +28,7 @@ server.listen(PORT, () => {
   console.log(`Server is running http://localhost:${PORT}`);
 });
 
-//Leer/Listar todas las entradas existentes
+//Leer/Listar todas las entradas existentes.
 server.get('/libros_disponibles', async (req,res)=>{
   try {
     const conex = await connectDB();
@@ -61,6 +61,55 @@ server.post('/nuevo_libro', async (req,res)=>{
       res.status(400).json({
         success: false,
         message: 'Libro no insertado en la base de datos.'
+      })
+    }
+    
+  } catch (error) {
+    res.status(500).json(error);
+  } 
+});
+
+//Actualizar una entrada existente.
+server.put('/libros_disponibles/:idLibro', async (req,res)=>{
+  try {
+    const conex = await connectDB();
+    const { titulo, fecha_publi, num_paginas } = req.body;
+    const { idLibro } = req.params;
+    const updateBook = 'UPDATE libros SET titulo=?, fecha_publi=?, num_paginas=? WHERE idLibro=?';
+    const [resultUpdateBook] = await conex.query(updateBook, [titulo, fecha_publi, num_paginas, idLibro]);
+    conex.end();
+    if(resultUpdateBook.affectedRows > 0){
+      res.status(201).json({
+      success: true
+    });
+    }else{
+      res.status(400).json({
+        success: false,
+        message: 'Libro no actualizado en la base de datos.'
+      })
+    }
+    
+  } catch (error) {
+    res.status(500).json(error);
+  } 
+});
+
+//Eliminar una entrada existente.
+server.delete('/libros_disponibles/:idLibro', async (req,res)=>{
+  try {
+    const conex = await connectDB();
+    const { idLibro } = req.params;
+    const deleteBook = 'DELETE FROM libros WHERE idLibro=?';
+    const [resultDeleteBook] = await conex.query(deleteBook, [idLibro]);
+    conex.end();
+    if(resultDeleteBook.affectedRows > 0){
+      res.status(201).json({
+      success: true
+    });
+    }else{
+      res.status(400).json({
+        success: false,
+        message: 'Libro no eliminado de la base de datos.'
       })
     }
     
